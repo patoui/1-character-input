3874b61800b9d36561ad92d1626e31a8d7b12fe2
Patrique Ouimet <patrique.ouimet@gmail.com>
2016-12-05 07:49:44 -0500

    Completed exercise 1 character input

---
+++ character-input.py
@@ -0,0 +1,3 @@
+user_name = raw_input("Please enter your name: ")
+user_age = int(raw_input("Please enter your age: "))
+print "Your name is " + user_name + " and in " + str(100 - user_age) + " you will be 100"
